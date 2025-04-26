547a5297bd6d4b8386b912f3fa1099638270023a
jAzz-hub <joaogu.2001@gmail.com>
2025-04-26 11:12:00 -0300

    <feat> :: Aceitando a leitura de números do tipo float

--- ferramentas.py
+++ ferramentas.py
@@ -1,10 +1,14 @@
-from tokenizer import isDelimitador, isOperadorAritmetico, isOperadorComparacao
 
-
-def digito_hexadecimal(caracter):
-    return caracter.isnumeric() or caracter in ['A', 'B', 'C', 'D', 'E', 'F']
-
-
-# Função que verifica se o pode ser aceito após o hexadecimal
-def caracterAceitoAposHexadecimal(caracter):
-    return isDelimitador(caracter) or isOperadorAritmetico(caracter) or isOperadorComparacao(caracter)
+def lendo_float(char_antecessor, char_atual, char_sucessor):
+    """
+    Verifica se um float em pascal está sendo lido pelo interpretador com base no último, próximo e atual caracteres lidos.
+    :param char_antecessor: Último caracter à ser lido.
+    :param char_atual: Caractere atual.
+    :param char_sucessor: Próximo caracter à ser lido.
+    :return: True se for um ponto flutuante, False caso contrário.
+    """
+    vizinhos_numericos = char_antecessor.isnumeric() and char_sucessor.isnumeric()
+    if char_atual == '.' and vizinhos_numericos:
+        return True
+    else:
+        return False
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from LeitorArquivos import LeitorArquivos
 from tokenizer import *
 from output import *
 from erros import *
+from ferramentas import lendo_float
 
 with open('output.txt', 'w', encoding='utf-8') as f:
     f.write(f"Código | Item | Linha | Coluna\n")
@@ -9,7 +10,7 @@ with open('output.txt', 'w', encoding='utf-8') as f:
 
 Leitor = LeitorArquivos()
 Leitor.LerArquivos()
-arquivo = Leitor.get_lines_program('EXS1.pas')
+arquivo = Leitor.get_lines_program('EXS27.pas')
 
 # Variáveis de estado
 palavra = ''
@@ -31,35 +32,47 @@ for linha in arquivo:
     
     while i < tamanho_linha:
         
-        caracter = linha[i]
+        caracter_atual= linha[i]
         
+        if i>0:
+            ultimo_caracter_lido = linha[i-1]
+        else: 
+            ultimo_caracter_lido = ''        
+
+        if i + 1 < tamanho_linha:
+            proximo_caracter = linha[i+1]
+        else:
+                proximo_caracter = ''
+
+        verificador_float = lendo_float(ultimo_caracter_lido, caracter_atual, proximo_caracter)
+
         # Tratamento de comentários
         if not aspas_dupla and not aspas_simples:
 
             if em_comentario:
 
-                if caracter == '}':
+                if caracter_atual== '}':
                     em_comentario = False
                 i += 1
                 cont_coluna += 1
                 continue
             
-            elif caracter == '{':
+            elif caracter_atual== '{':
                 em_comentario = True
                 i += 1
                 cont_coluna += 1
                 continue
-            elif caracter == '/' and i + 1 < tamanho_linha and linha[i+1] == '/':
+            elif caracter_atual== '/' and i + 1 < tamanho_linha and linha[i+1] == '/':
                 break  # Pula o resto da linha
 
-        # Se estiver em comentário, ignora o caractere   
+        # Se estiver em comentário, ignora ocaracter_atuale   
         if em_comentario:
             i += 1
             cont_coluna += 1
             continue
             
         # Tratamento de strings
-        if caracter == '"' and not aspas_simples:
+        if caracter_atual== '"' and not aspas_simples:
             if aspas_dupla:
                 # Fechamento de string
                 write_output('string', string_buffer, cont_linha, cont_coluna - len(string_buffer) - escape_coluna)
@@ -73,7 +86,7 @@ for linha in arquivo:
             cont_coluna += 1
             continue
             
-        elif caracter == "'" and not aspas_dupla:
+        elif caracter_atual== "'" and not aspas_dupla:
             if aspas_simples:
                 # Fechamento de string
                 write_output('string', string_buffer, cont_linha, cont_coluna - len(string_buffer) - escape_coluna)
@@ -89,7 +102,7 @@ for linha in arquivo:
             
         # Se estiver dentro de string (tratamento escape '\')
         if aspas_dupla or aspas_simples:
-            if caracter == '\\':  # Caractere de escape
+            if caracter_atual== '\\':  # Caractere de escape
                 escape_coluna += 1
                 if i + 1 < tamanho_linha:
                     string_buffer += linha[i+1]
@@ -99,26 +112,26 @@ for linha in arquivo:
                 else:
                     string_buffer += '\\'
             else:
-                string_buffer += caracter
+                string_buffer +=caracter_atual
             i += 1
             cont_coluna += 1
             continue
             
         # Tratamento de operadores aninhados
-        if caracter in [':', '<', '>', '=', '/', '*'] and operador_aninhado == '':
-            operador_aninhado = caracter
+        if caracter_atual in [':', '<', '>', '=', '/', '*'] and operador_aninhado == '':
+            operador_aninhado = caracter_atual
             i += 1
             cont_coluna += 1
             continue
             
         if operador_aninhado:
-            if caracter in ['=', '>']:
-                operador_aninhado += caracter
+            if caracter_atual in ['=', '>']:
+                operador_aninhado += caracter_atual
                 write_output('operador', operador_aninhado, cont_linha, cont_coluna - len(operador_aninhado) + 1)
                 if palavra: 
                     tam_operador_aninhado = len(operador_aninhado)
                 else:
-                    tam_operador_aninhado=0
+                    tam_operador_aninhado = 0
                 operador_aninhado = ''
                 i += 1
                 cont_coluna += 1
@@ -128,12 +141,12 @@ for linha in arquivo:
                 if palavra: 
                     tam_operador_aninhado = len(operador_aninhado)
                 else:
-                    tam_operador_aninhado=0
+                    tam_operador_aninhado = 0
                 operador_aninhado = ''
                 continue
        
         # Tratamento de delimitadores
-        if isDelimitador(caracter):
+        if isDelimitador(caracter_atual) and verificador_float is False:
             if palavra:
                 # Processa a palavra acumulada
                 if (palavra in palavras_reservadas) or (palavra in operadores_logicos) or (palavra in io_tokens) or (palavra in condicionais):
@@ -143,14 +156,14 @@ for linha in arquivo:
                     tam_operador_aninhado=0
                 palavra = ''
             
-            if caracter not in [' ', '\t', '\n']:  # Ignora espaços em branco
-                write_output('delimitador', caracter, cont_linha, cont_coluna)
+            if caracter_atual not in [' ', '\t', '\n']:  # Ignora espaços em branco
+                write_output('delimitador',caracter_atual, cont_linha, cont_coluna)
             i += 1
             cont_coluna += 1
             continue
             
         # Acumula caracteres para formar palavras
-        palavra += caracter
+        palavra +=caracter_atual
         i += 1
         cont_coluna += 1
     
