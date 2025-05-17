1184cb1db80258f84bd73726576bd4adbf3b2178
jAzz-hub <joaogu.2001@gmail.com>
2025-05-17 15:01:47 -0300

    <feat> :: Implementação de Instruções dos programas | Fase: Analisador sintático

--- src/sintatico.py
+++ src/sintatico.py
@@ -1,5 +1,12 @@
 from output import dicionario_tokens
+from tokenizer import
+{
+    io_tokens    
+}
 
+#---------------------------------------------------
+#       DECLARAÇÕES DAS VARIÁVEIS
+#---------------------------------------------------
 def consome(codigo, lista):
     if lista[0][0] == dicionario_tokens[codigo]:
         lista.pop(0)
@@ -46,6 +53,81 @@ def declarations(lista):
     declaration(lista)
     restoDeclaration(lista)
 
+def endFor(lista):
+    if lista[0][0] == 'IDENT':
+        consome('IDENT', lista)
+    elif lista[0][0] == 'NUMint':
+        consome('NUMint', lista)
+    else:
+        print(f"Erro sintático! Esperado: integer ou real ou string | Recebido: {lista[0][1]} | Linha: {lista[0][2]} | Coluna: {lista[0][3]}")
+        exit(1)
+
+
+
+# ---------------------------------------------------
+#       INTRUÇÕES DOS PROGRAMAS
+# ---------------------------------------------------
+# def ioStmt(lista):
+#     if lista[0][0] == 'read':
+#         consome('read', lista)
+#         consome('(', lista)
+#         consome('IDENT', lista)
+#         consome(')', lista)
+#         consome(';', lista)
+#     if lista[0][0] in set('write', 'writeln'):
+#         consome('read', lista)
+#         consome('(', lista)
+#         outlist(lista)
+#         consome(')', lista)
+#         consome(';', lista)
+#     if lista[0][0] == 'readln':
+#         consome('read', lista)
+#         consome('(', lista)
+#         consome('IDENT', lista)
+#         consome(')', lista)
+#         consome(';', lista)
+
+    
+def stmt(lista):
+    if lista[0][0] in io_tokens:
+        # ioStmt(lista)
+    elif lista[0][0] == 'while':
+        # whileStmt(lista)
+    elif lista[0][0] == ':=':
+        atrib(lista)
+        consome(';', lista)
+    elif lista[0][0] == 'if':
+        # ifStmt(lista)
+    elif lista[0][0] == 'break'
+        # break_pascal()
+        consome(';', lista)
+    elif lista[0][0] == 'continue'
+        continue_pascal()
+        consome(';', lista)
+    else:
+        if lista[0][0] == 'begin':
+            bloco()
+        else:
+            consome(';', lista)
+
+
+def stmtList(lista):
+    valor = lista[0][0]
+
+    casos_de_bloco = {'while', 'if', ':=', 'begin', 'break', 'continue'}
+
+    if lista[0][0] in casos_de_bloco:
+        stmt(lista)
+        stmtList(lista)
+
+
+def bloco(lista):
+    consome('begin', lista)
+    stmtList(lista)
+    consome('end', lista)
+    consome(';', lista)
+
+
 def sintatico(lista):
     consome('program', lista)
     consome('variavel', lista)
@@ -55,3 +137,8 @@ def sintatico(lista):
     
     if len(lista) == 0:
         print("Análise sintática concluída com sucesso")
+    else:
+        bloco(lista)
+        
+        if len(lista) == 0:
+            print("Análise sintática concluída com sucesso")
