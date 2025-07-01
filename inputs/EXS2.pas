68eb73e22a2a4f761b49d6295c5a109fa892df0b
Julia Rezende Rocha <84331927+juliarezende34@users.noreply.github.com>
2025-07-01 16:15:50 -0300

    corrigindo condição no input 2

--- inputs/EXS2.pas
+++ inputs/EXS2.pas
@@ -8,7 +8,7 @@ begin
     read(x);
     read(y);
 
-    if (x > y) and (y <> 1) then
+    if (x > y) and (y <> 0) then
         writeln('X é maior e Y não é zero');
     else
         write('Condição falsa');
